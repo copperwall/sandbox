ee6db2af99f89142aa5353f9c10febacb9ed07ac
Chris Opperwall <chris@truepill.com>
2020-06-10 09:58:59 -0700

    feat: add another lol

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import something from './util'
 
-export const sandbox = (...args) => console.log(`called with ${args}`)
+export const sandbox = (...args) => console.log(`called with ${args} lol`)
 export const thingy = 'lol'
